80acbf44c0a8867f6575f12756368e69b6f9487e
Chih-Horng Ke <kchlmp@gmail.com>
2019-04-29 12:52:30 +0800

    add class04

---
+++ class04/class04/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  class04
+//
+//  Created by kech-MacbookPro15 on 2019/4/29.
+//  Copyright © 2019 kech-MacbookPro15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ class04/class04Tests/class04Tests.swift
@@ -0,0 +1,34 @@
+//
+//  class04Tests.swift
+//  class04Tests
+//
+//  Created by kech-MacbookPro15 on 2019/4/29.
+//  Copyright © 2019 kech-MacbookPro15. All rights reserved.
+//
+
+import XCTest
+@testable import class04
+
+class class04Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
